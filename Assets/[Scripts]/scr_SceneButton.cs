ef68507d59b680be439f1193268b42e258fa9cff
Sam <samcpollock@gmail.com>
2021-10-01 15:20:34 -0400

    Added button functionality
    - Made button script to change scenes 
    - Added "TESTING_LoseButton" to gameplay scene.

--- Assets/[Scripts]/scr_SceneButton.cs
+++ Assets/[Scripts]/scr_SceneButton.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class scr_SceneButton : MonoBehaviour
 {
+    [SerializeField]
+    //int targetSceneIndex;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +18,10 @@ public class scr_SceneButton : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnSceneChangeButtonPressed(int targetSceneIndex)
+    {
+        SceneManager.LoadScene(targetSceneIndex);
     }
 }
